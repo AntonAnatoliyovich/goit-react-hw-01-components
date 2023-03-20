8611ce79de0ca372dcfcc60e48eb305bb3aecbbd
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-03-20 08:49:39 +0200

    init

--- src/components/FriendList/friendListItem.jsx
+++ src/components/FriendList/friendListItem.jsx
@@ -3,7 +3,7 @@ import { FriendItem, FriendSpan, FriendAvatar, FriendName } from './friendListIt
 export const FriendListItem = ({ id, isOnline, avatar, name }) => {
     return (
         <FriendItem key={id}>
-            <FriendSpan className={"status " + (isOnline === true ? "active" : "")}>{isOnline}</FriendSpan>
+            <FriendSpan className={"status " + (isOnline === true ? "active" : "")}></FriendSpan>
             <FriendAvatar className="avatar" src={avatar} alt="User avatar" width="48" />
             <FriendName className="name">{name}</FriendName>
         </FriendItem>
